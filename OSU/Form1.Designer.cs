c9e6d69b5625ac710306fc11d48811f103eb4e0a
oleg <olegzaytsev12@mail.ru>
2023-11-18 16:09:50 +0300

    first

---
+++ OSU/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace OSU
+{
+    partial class OSU
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OSU));
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scorelabl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 25;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // scorelabl
+            // 
+            this.scorelabl.AutoSize = true;
+            this.scorelabl.BackColor = System.Drawing.Color.Indigo;
+            this.scorelabl.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.scorelabl.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scorelabl.Location = new System.Drawing.Point(1026, 9);
+            this.scorelabl.Name = "scorelabl";
+            this.scorelabl.Size = new System.Drawing.Size(38, 45);
+            this.scorelabl.TabIndex = 0;
+            this.scorelabl.Text = "0";
+            // 
+            // OSU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::OSU.Resource1.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1106, 561);
+            this.Controls.Add(this.scorelabl);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OSU";
+            this.Text = "OSU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label scorelabl;
+    }
+}
+
---
+++ OSU/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSU
+{
+    public partial class OSU : Form
+    {
+        public Bitmap HandlerTexture = Resource1.Handler,
+                      TargetTexture = Resource1.Target;
+        private Point _TagretPosition = new Point(300, 300);
+        private Point _direction = Point.Empty;
+        private int _score = 0;
+        public OSU()
+        {
+            InitializeComponent();
+
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer1.Interval = r.Next(25, 1000);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            var LocalPosition = this.PointToClient(Cursor.Position);
+
+            _TagretPosition.X += _direction.X * 10;
+            _TagretPosition.Y += _direction.Y * 10;
+
+            if (_TagretPosition.X < 0 || _TagretPosition.X > 500)
+            {
+                _direction.X *= -1;
+            }
+            if (_TagretPosition.Y < 0 || _TagretPosition.Y > 500)
+            {
+                _direction.Y *= -1;
+            }
+
+            Point between = new Point(LocalPosition.X - _TagretPosition.X, LocalPosition.Y - _TagretPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+
+            if (distance < 20)
+            {
+                AddScore(1);
+            }
+
+
+            var handlerRect = new Rectangle(LocalPosition.X - 50, LocalPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_TagretPosition.X - 50, _TagretPosition.Y - 50, 100, 100);
+
+
+            g.DrawImage(HandlerTexture, handlerRect);
+            g.DrawImage(TargetTexture, targetRect);
+        }   
+        private void AddScore(int score)
+        {
+            _score += score;
+            scorelabl.Text = _score.ToString();
+        }
+    }
+}
